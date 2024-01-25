24519b30493d4b59081124fd147c1e56641fb630
Abhishek Kumar Gupta <badabhishekgupta@gmail.com>
2024-01-25 21:53:27 +0530

    Completed data module...

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<PizzaStoreContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
@@ -16,5 +27,27 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<PizzaStoreContext>();
+
+    if (context.Database.CanConnect())
+    {
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+        else
+        {
+            if (!context.Specials.Any())
+            {
+                SeedData.Initialize(context);
+            }
+        }
+    }
+}
 
 app.Run();
